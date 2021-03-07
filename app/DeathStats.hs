bc214fc29ce47d0a1386cd11cb0eaadd18fae002
Mads Buch <me@madsbuch.com>
2021-03-07 10:47:28 +0100

    Chore: Refactor

--- app/DeathStats.hs
+++ app/DeathStats.hs
@@ -4,7 +4,29 @@ import Fog
 import Control.Monad.Probability
 import qualified System.Random as R
 import DeathTypes
+import DeathData
 
+report = do
+  putStrLn "Objective: Verify following"
+  putStrLn ""
+  putStrLn "> The average Dane is expected to live seventy-seven years."
+  putStrLn "> Therefore if you are seventy-two you can expect to live five more years,"
+  putStrLn "> and should plan accordingly."
+  putStrLn ""
+
+  -- Our observations
+  o <- observations
+  let dist = deathRealm o
+
+  putStr "Life Expectancy: "
+  let a = expectedAge dist
+  putStrLn $ show a
+
+  putStr "Life Expectancy at 77: "
+  let a = expectedAge $ deathRealmPerAge dist 77
+  putStrLn $ show a
+
+-- Helper functions
 
 expectationBySampling :: (Ord a) => (a -> Double) -> P a -> Double
 expectationBySampling f p = let
--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,6 @@
 module Main where
 
 import DeathStats
-import DeathData
 
 main :: IO ()
-main = do
-  putStrLn "Objective: Verify following"
-  putStrLn ""
-  putStrLn "> The average Dane is expected to live seventy-seven years."
-  putStrLn "> Therefore if you are seventy-two you can expect to live five more years,"
-  putStrLn "> and should plan accordingly."
-  putStrLn ""
-
-  -- Our observations
-  o <- observations
-  let dist = deathRealm o
-
-  putStr "Life Expectancy: "
-  let a = expectedAge dist
-  putStrLn $ show a
-
-  putStr "Life Expectancy at 77: "
-  let a = expectedAge $ deathRealmPerAge dist 77
-  putStrLn $ show a
+main = report
